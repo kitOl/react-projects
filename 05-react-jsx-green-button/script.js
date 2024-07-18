cae701c7221ae21c89027f250f6472820769b086
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-07-19 02:18:45 +0300

    add useState for change color button

--- 05-react-jsx-green-button/script.js
+++ 05-react-jsx-green-button/script.js
@@ -1,13 +1,20 @@
 const App = () => {
   const [buttonText, setButtonText] = React.useState("Click me");
+  const [buttonColor, setButtonColor] = React.useState("");
+  console.log(buttonColor);
+
   const onButtonClick = () => {
     setButtonText("Hello from React");
     // console.log("button text", buttonText);
+    setButtonColor("green-btn");
+    // setButtonColor(`green-btn ${Math.random()}`);
   };
 
   return (
     <div className="app">
-      <button onClick={onButtonClick}>{buttonText}</button>
+      <button className={buttonColor} onClick={onButtonClick}>
+        {buttonText}
+      </button>
     </div>
   );
 };
