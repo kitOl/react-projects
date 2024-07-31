16f92bdf16ebcbb89e5ad27e617acebc31b40a7d
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-07-31 21:17:41 +0300

    Filter by author

--- 30-react-redux/frontend/src/redux/slices/filterSlice.js
+++ 30-react-redux/frontend/src/redux/slices/filterSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   title: "",
+  author: "",
 };
 
 const filterSlice = createSlice({
@@ -13,11 +14,17 @@ const filterSlice = createSlice({
       // return { ...state, title: action.payload };
     },
     resetFilters: (state) => initialState,
+    setAuthorFilter: (state, action) => {
+      state.author = action.payload;
+    },
   },
 });
 
-export const { setTitleFilter, resetFilters } = filterSlice.actions;
+export const { setTitleFilter, setAuthorFilter, resetFilters } =
+  filterSlice.actions;
 
 export const selectTitleFilter = (state) => state.filter.title;
+
+export const selectAuthorFilter = (state) => state.filter.author;
 
 export default filterSlice.reducer;
