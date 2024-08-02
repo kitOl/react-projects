c5859a0255225e12a64efba4da200d6488cf66ce
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-08-02 06:06:47 +0300

    Favorite checkbox filter

--- 30-react-redux/frontend/src/redux/slices/filterSlice.js
+++ 30-react-redux/frontend/src/redux/slices/filterSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   title: "",
   author: "",
+  onlyFavorite: false,
 };
 
 const filterSlice = createSlice({
@@ -13,18 +14,24 @@ const filterSlice = createSlice({
       state.title = action.payload;
       // return { ...state, title: action.payload };
     },
-    resetFilters: (state) => initialState,
     setAuthorFilter: (state, action) => {
       state.author = action.payload;
     },
+    setOnlyFavoriteFilter: (state) => {
+      state.onlyFavorite = !state.onlyFavorite;
+    },
+    resetFilters: () => initialState,
   },
 });
-
-export const { setTitleFilter, setAuthorFilter, resetFilters } =
-  filterSlice.actions;
+export const {
+  setTitleFilter,
+  setAuthorFilter,
+  setOnlyFavoriteFilter,
+  resetFilters,
+} = filterSlice.actions;
 
 export const selectTitleFilter = (state) => state.filter.title;
-
 export const selectAuthorFilter = (state) => state.filter.author;
+export const selectOnlyFavoriteFilter = (state) => state.filter.onlyFavorite;
 
 export default filterSlice.reducer;
